2f762c59bd3f4c500568153e6bce717026817981
lucvs5 <lucasrosaskt@gmail.com>
2026-03-07 16:03:21 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,18 @@
-// -----------------------------
 // MODAL DE LOGIN AO CLICAR NO CARRINHO
-// -----------------------------
-
-// Seleciona elementos
 const modal = document.getElementById("cart-modal");
 const cartBtn = document.querySelector(".cart-icon");
 const closeBtn = document.querySelector(".close-modal");
 
-// Abre o modal ao clicar no carrinho
 cartBtn.addEventListener("click", () => {
-  modal.style.display = "flex";
+  modal.style.display = "flex"; // mostra o modal
 });
 
-// Fecha o modal ao clicar no X
 closeBtn.addEventListener("click", () => {
-  modal.style.display = "none";
+  modal.style.display = "none"; // fecha o modal
 });
 
-// Fecha o modal ao clicar fora da janela
 window.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    modal.style.display = "none";
+  if(e.target === modal){
+    modal.style.display = "none"; // fecha ao clicar fora
   }
 });
