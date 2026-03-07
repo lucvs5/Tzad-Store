ca8b016b907159803cb5ca62925a32717aacc660
lucvs5 <lucasrosaskt@gmail.com>
2026-03-07 04:07:37 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ console.log("Tzad Store carregada");
 // Topo que desce quando a página é rolada
 const topo = document.querySelector('.topo');
 
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    topo.style.transform = 'translateY(-80px)'; // desce
+    topo.style.transition = 'transform 0.3s ease';
+  } else {
+    topo.style.transform = 'translateY(0)'; // volta ao topo
+  }
+});
