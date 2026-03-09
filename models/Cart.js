022a60cf21d21c1a7e59555ca7c5f96b6337787f
lucvs5 <lucasrosaskt@gmail.com>
2026-03-09 07:02:07 -0300

    Update Cart.js

--- models/Cart.js
+++ models/Cart.js
@@ -1,1 +1,20 @@
+const mongoose = require("mongoose");
+
+const CartSchema = new mongoose.Schema({
+
+userId:String,
+
+productId:String,
+
+name:String,
+
+image:String,
+
+price:Number,
+
+quantity:Number
+
+});
+
+module.exports = mongoose.model("Cart",CartSchema);
 
