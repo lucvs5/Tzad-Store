2324288e35ff343824b51f5f742b7d1fc8c7b95c
lucvs5 <lucasrosaskt@gmail.com>
2026-03-11 05:25:11 -0300

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,6 +1,1 @@
-window.addEventListener('load', renderVitrines);
 
-function abrirModalCompra(id, cat) {
-    const product = catalog[cat].find(p => p.id === id);
-    if (window.abrirModal) window.abrirModal(product);
-}
